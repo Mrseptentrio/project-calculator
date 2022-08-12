b93ebe61be1f6a80decefe5944df27732a963946
Mrseptentrio <mrseptentrio@gmail.com>
2022-08-12 14:18:48 +0100

    follow up

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+const previousOperation = document.querySelector('[data-previous-opeation]')
+const currentOperation = document.querySelector('[data-current-operation]')
+const tempOperation = document.querySelector('[data-temp-operation]')
+const equalsBtn = document.querySelector('[data-equals]')
+const numberBtn = document.querySelectorAll('[data-number]')
+const operatorBtn = document.querySelectorAll('[data-operator]')
+const allClearBtn = document.querySelector('[data-all-clear]')
+const deleteBtn = document.querySelector('[data-delete]')
+
+let currentDisplayedOperation = '';
+let previousDisplayedOperation = '';
+let result = null;
+let lastOperation = '';
+let haveDot = false;
+
+numberBtn.forEach(number => {
+  number.addEventListener('click', (e) => {
+    if (e.target.innerText === '.' && !haveDot) {
+      haveDot = true
+    } else if (e.target.innerText === '.' && haveDot) {
+      return;
+    }
+    currentDisplayedOperation += e.target.innerText;
+    currentOperation.innerText = currentDisplayedOperation;
+  })
+})
+
+
+operatorBtn.forEach(operator => {
+  operator.addEventListener('click', (e) => {
+    if (!previousDisplayedOperation) return;
+    haveDot = false;
+    const operationName = e.target.innerText;
+    if (currentDisplayedOperation && previousDisplayedOperation && lastOperation) {
+      mathOperation();
+    } else {
+      result = parseFloat(previousDisplayedOperation);
+    }
+    console.log(result)
+  })
+})
+
 
 
 
@@ -17,7 +59,5 @@ function divide(a, b) {
   return a / b;
 };
 
-function result() {
 
-};
 
